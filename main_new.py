add278c124312f33e41553cc3979f824e7b4290b
Isaac Sheidlower <qigloos@gmail.com>
2022-07-01 13:43:06 -0400

    added learning

---
+++ main_new.py
@@ -0,0 +1,126 @@
+"""
+This code mainly follows a Soft-Actor Critic YouTube tutorial found at:
+https://www.youtube.com/watch?v=ioidsRlf79o&t=2649s
+Channel name: Machine Learning with Phil
+
+Any modifiations are made by the AABL Lab.
+"""
+
+from ast import expr_context
+import gym
+import numpy as np
+from sac_torch_costom_new import Agent
+import time
+import pickle
+
+from tensorboardX import SummaryWriter
+
+if __name__ == '__main__':
+    env = gym.make('BipedalWalker-v3')
+    #BipedalWalker-v3
+    print(env.observation_space.shape)
+    in_dims = (25,)
+    #agent = Agent(alpha=.0003, beta=.0003, disc_lr=.0001, input_dims=in_dims, env=env, batch_size=256, disc_layer1_size=256, disc_layer2_size=256,
+    #        tau=.02, max_size=100000, layer1_size=400, layer2_size=300, n_actions=env.action_space.shape[0], reward_scale=15, auto_entropy=True, disc_input_dims=(1,), predict_dims=1)
+    agent = pickle.load(open("/content/drive/MyDrive/costum_rl_agents/SA_15rew_speed_10MSEandp1_sparEnvRew_cdfOnly_withMaxDiff_from3500_indexRew_8000.p", "rb" ))
+    agent.actor.max_action=1
+    n_games = 10001
+    rewards = []
+    # uncomment this line and do a mkdir tmp && mkdir video if you want to
+    # record video of the agent playing the game.
+    #env = wrappers.Monitor(env, 'tmp/video', video_callable=lambda episode_id: True, force=True)
+    #filename = 'LunarLanderContinuous.png'
+
+    #figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+        env.render(mode='human')
+
+    render = False
+    env_interacts = 0
+
+    writer = SummaryWriter()
+    action = env.action_space.sample()
+    for i in range(8001, n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        limit_factor = np.random.uniform(low=0, high=1)
+        #limit_factor2 = np.random.uniform(low=0, high=1)
+        observation = np.concatenate((observation, [limit_factor]))
+        while not done:
+            env_interacts+=1
+            #agent.actor.max_action = limit_factor
+            #try:
+            action = agent.choose_action(observation)
+            #except:
+            #    pass
+            observation_, reward, done, info = env.step(action)
+            observation_ = np.concatenate((observation_, [limit_factor]))
+            score += reward
+            agent.remember(observation, action, reward, observation_, done)
+            #if not load_checkpoint:
+                #if env_interacts > 1000:
+            #if env_interacts % 400:
+            #    limit_factor = np.random.uniform(low=0, high=1)
+            observation[-1] = limit_factor
+            if env_interacts % 1000 == 0:
+                try:
+                    act_loss, disc1_loss, disc1_log_probs, \
+                        disc1_crit = agent.learn(update_params=True, update_disc=True)
+                    if act_loss is not None and disc1_loss is not None:
+                        writer.add_scalar("Loss/act_new", np.mean(act_loss.item()),env_interacts)
+                        writer.add_scalar("Loss/disc1_loss_new", np.mean(disc1_loss.item()),env_interacts)
+                        writer.add_scalar("Loss/disc1_crit_new", disc1_crit,env_interacts)
+                        writer.add_scalar("Loss/disc1_log_pob_new", disc1_log_probs,env_interacts)
+                        
+                except:
+                    pass
+            else:        
+                try:
+                    act_loss, _ = agent.learn()
+                    #if act_loss is not None:
+                        #writer.add_scalar(f"Loss/act_loss_366_14", np.mean(act_loss.item()),env_interacts)
+                        #writer.add_scalar("Loss/disc_loss", np.mean(disc_loss.item()),env_interacts)
+                except Exception as e:
+                    print(e)
+                    raise
+
+            #if env_interacts % 100 == 0:
+                #limit_factor = np.random.uniform(low=0, high=1)
+                #limit_factor2 = np.random.uniform(low=0, high=1)
+    
+            """if(i>=0):
+                env.render()
+                print(observation[4], observation[6] )
+                time.sleep(2)"""
+            #if render:
+            #env.render()
+            observation = observation_
+            #limit_factor = np.random.uniform(low=.4, high=1)
+            #env.render()
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            #if not load_checkpoint:
+            #    agent.save_models()
+
+        rewards.append(score)
+
+        #if score > 298:
+        #    break
+
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f, ' % avg_score, "limit_factor %.2f" % limit_factor,  ", 0003, 0001, 378 100 update")
+
+        if i % 500 == 0 and i > 1400:
+            pickle.dump(agent, open( f"/content/drive/MyDrive/costum_rl_agents/SA_15rew_speed_10MSEandp1_sparEnvRew_cdfOnly_withMaxDiff_from3500_indexRew_100update_{i}.p", "wb" ) )
+    
+    #pickle.dump(agent, open( "speedAngle13rew_visi5_3500.p", "wb" ) )
+    #np.save("BP_sac_2000", rewards)
---
+++ slider_testing.py
@@ -0,0 +1,121 @@
+import numpy as np
+from tkinter import *
+import tkinter as tk
+import time, pickle
+import threading
+import gym
+
+class App(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
+        self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+
+        
+        self.w = Scale(self.root, from_=0, to=1, orient=HORIZONTAL, tickinterval=.0001, resolution=.0001)
+        self.w.pack()
+
+
+    def callback(self):
+        self.root.quit()
+
+    def run(self):
+        self.root.update()
+
+    def get_w(self):
+        return self.w.get()
+
+
+app = App()
+#app2 = App()
+for i in range(100000):
+    
+    env = gym.make('BipedalWalker-v3')
+    #LunarLanderContinuous-v2
+    #agent = Agent(alpha=0.000314854, beta=0.000314854, input_dims=env.observation_space.shape, env=env, batch_size=128,
+    #        tau=.02, max_size=50000, layer1_size=400, layer2_size=300, n_actions=env.action_space.shape[0], reward_scale=1, auto_entropy=True,max_action=.5)
+    agent = pickle.load(open("agents\SA_15rew_speed_10MSEandp1_sparEnvRew_cdfOnly_withMaxDiff_from3500_indexRew_8000.p", "rb" ))
+    print(agent.scale)
+    #agent.actor.max_action=.1
+    n_games = 3000
+    rewards = []
+    # uncomment this line and do a mkdir tmp && mkdir video if you want to
+    # record video of the agent playing the game.
+    #env = wrappers.Monitor(env, 'tmp/video', video_callable=lambda episode_id: True, force=True)
+    filename = 'LunarLanderContinuous.png'
+
+    figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+        env.render(mode='human')
+    
+    limit_factor = 0.1
+    limit_factor2 = 0.0
+
+    render = False
+    env_interacts = 0
+    for i in range(n_games):
+        speed = []
+        yvel=[]
+        observation = env.reset()
+        observation = np.concatenate((observation, [limit_factor]))
+        done = False
+        score = 0
+        while not done:
+            app.run()
+            #app2.run()
+            limit_factor = app.w.get()
+            #limit_factor2 = app2.w.get()
+            env_interacts+=1
+            #if env_interacts%300 == 0:
+            #    print("INTERACT", env_interacts)
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            observation_ = np.concatenate((observation_, [limit_factor]))
+            score += reward
+            speed.append(observation[2])
+            yvel.append(observation[0])
+            #agent.remember(observation, action, reward, observation_, done)
+            #if not load_checkpoint:
+                #if env_interacts > 1000:
+                    #if env_interacts % 128 == 0:
+            #    agent.learn(update_params=True)
+                    #else:
+                        #agent.learn()
+            """if(i>=0):
+                env.render()
+                print(observation[4], observation[6] )
+                time.sleep(2)"""
+            #if render:
+            env.render()
+            observation = observation_
+            #env.render()
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            #if not load_checkpoint:
+            #    agent.save_models()
+
+        rewards.append(score)
+
+        #if score > 298:
+        #    break
+        #print(limit_factor, limit_factor2)
+        print(limit_factor)
+        #print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score, f"speed: {np.mean(speed)}", f"yvel: {np.mean(yvel)}")
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score, f"speed: {np.mean(speed)}")
+
+
+
+
+mainloop()
+
